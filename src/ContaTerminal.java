47f21c46e72d363e6154101d52b27569ef696f5e
Uelinton <79346985+uelintonn@users.noreply.github.com>
2024-05-17 17:16:52 -0300

    Iniciando ContaBanco

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,14 @@
 public class ContaTerminal {
     public static void main(String[] args) {
+
+       int numero = 1021;
+       String agencia;
+       String nomeDoCliente;
+       double saldo = 237.48;
+
+
+
+
         System.out.println("Hello");
     }
 }
