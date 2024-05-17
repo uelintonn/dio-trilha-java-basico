d1a9754eb258d1058d4b2df171449344dca4fa51
Uelinton <79346985+uelintonn@users.noreply.github.com>
2024-05-17 17:59:18 -0300

    Projeto finalizado.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,14 +1,39 @@
+import java.util.Scanner;
+
+
 public class ContaTerminal {
     public static void main(String[] args) {
 
-       int numero = 1021;
+
+       int numero;
        String agencia;
        String nomeDoCliente;
-       double saldo = 237.48;
+        double saldo;
+
+        Scanner scanner = new Scanner(System.in);
 
 
+        System.out.print("Digite o número da sua conta: ");
+        String numeroStr = scanner.nextLine();
+        try {
+            numero = Integer.parseInt(numeroStr);
+        } catch (NumberFormatException e) {
+            System.out.println("Erro: você deve digitar um número inteiro válido para o número da conta.");
+            scanner.close();
+            return;
+        }
 
 
-        System.out.println("Hello");
+        System.out.print("Digite o número da sua agência: ");
+        agencia = scanner.nextLine();
+
+
+        System.out.print("Digite o seu nome: ");
+        nomeDoCliente = scanner.nextLine();
+
+        System.out.print("Digite o seu saldo: ");
+         saldo = scanner.nextDouble();
+
+        System.out.println("O número da sua conta é :" + numeroStr + " Sua agência é " + agencia +"," +  "Nome do cliente é " + nomeDoCliente + "Seu saldo é:" + saldo) ;
     }
 }
